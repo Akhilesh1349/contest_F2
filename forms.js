c414fa038932b973fca960a48284246b5efa5aad
Akhilesh Tedlapu <104684152+Akhilesh1349@users.noreply.github.com>
2023-05-20 20:19:11 +0530

    Add files via upload

---
+++ forms.js
@@ -0,0 +1,8 @@
+function scrollToRegisterForm(){
+    var targetDiv = document.querySelector(".register_form");
+    targetDiv.scrollIntoView({ behavior: "smooth"});
+}
+function scrollToBooksTable(){
+    var targetDiv = document.querySelector(".books");
+    targetDiv.scrollIntoView({ behavior: "smooth"});
+}
